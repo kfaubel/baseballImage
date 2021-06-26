2ff275ec9d8d2370b9f511e67784e6406a1ee655
Ken Faubel <ken@faubel.org>
2021-06-26 16:40:34 -0400

    fix new path to js files in index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // This is used when embedding this in a larger solution
 // Use app.ts for local testing and to build a stand alone solution
 
-const BaseballScheduleImage = require('./build/src/baseballimage');
+const BaseballScheduleImage = require('./build/baseballimage');
 
 module.exports = BaseballScheduleImage;
