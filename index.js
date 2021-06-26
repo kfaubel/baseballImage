163da8ed23df70767a31dc6e0fe9828be7cfc126
Ken Faubel <ken@faubel.org>
2021-06-26 15:09:59 -0400

    fix reference to file

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // This is used when embedding this in a larger solution
 // Use app.ts for local testing and to build a stand alone solution
 
-const BaseballScheduleImage = require('./build/baseballimage');
+const BaseballScheduleImage = require('./build/src/baseballimage');
 
 module.exports = BaseballScheduleImage;
